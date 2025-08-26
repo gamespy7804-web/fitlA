54cff96a027d867a880d0e9e5202032bec5f161a
Games Py <gamespy7804@gmail.com>
2025-08-26 12:10:16 +0000

    Para preparar tu sitio para mostrar anuncios, copia y pega esta etiqueta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="es" className="dark">
       <head>
+        <meta name="google-adsense-account" content="ca-pub-9675156642757442" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
